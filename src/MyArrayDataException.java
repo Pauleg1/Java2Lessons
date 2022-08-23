fb61ffb833d7619b252940931bde9b4bf29bfc98
pa.berezin <89036170812p@gmail.com>
2022-08-23 21:41:16 +0300

    1

---
+++ src/MyArrayDataException.java
@@ -0,0 +1,5 @@
+public class MyArrayDataException extends NumberFormatException {
+    public MyArrayDataException(String s) {
+        super(s);
+    }
+}
---
+++ src/MyArraySizeException.java
@@ -0,0 +1,5 @@
+public class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
