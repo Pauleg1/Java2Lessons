ec1c7853b494ae9cb1f475db5128de951c354821
pa.berezin <89036170812p@gmail.com>
2022-08-23 21:49:51 +0300

    HW2

---
+++ src/HomeWork2/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package HomeWork2;
+
+public class MyArrayDataException extends NumberFormatException {
+    public MyArrayDataException(String s) {
+        super(s);
+    }
+}
---
+++ src/HomeWork2/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package HomeWork2;
+
+public class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
