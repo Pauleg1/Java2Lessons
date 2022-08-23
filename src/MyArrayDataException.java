5a5e62a131db9f922f0022bc6051309e4cca2e9c
pa.berezin <89036170812p@gmail.com>
2022-08-23 22:02:05 +0300

    выполнил HW2

--- src/MyArrayDataException.java
+++
@@ -1,5 +0,0 @@
-public class MyArrayDataException extends NumberFormatException {
-    public MyArrayDataException(String s) {
-        super(s);
-    }
-}
--- src/MyArraySizeException.java
+++
@@ -1,5 +0,0 @@
-public class MyArraySizeException extends Exception {
-    public MyArraySizeException(String message) {
-        super(message);
-    }
-}
